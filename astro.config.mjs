39267661c203d188e29d2ea0d8d27b929c5d3aea
Bianca Tuliao <biancatuliao@Axel-Lotl.local>
2026-07-23 02:26:43 -0500

    update files with images

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,8 @@ import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://mysite.com",
+  site: "https://beetul.github.io/",
+  base: "/beetul.github.io",
   devToolbar: {
     enabled: false,
   },
